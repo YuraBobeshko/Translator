ee22ddc984bce801b578f1087f2d2a55848b16d3
Yuri Bobeshko <gorobech55@gmail.com>
2020-12-19 11:32:47 +0200

    sol

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import getMedia from "./hook";
 
 export default function App() {
+  const [running, setRunning, features] = getMedia();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Button
+        onPress={() => setRunning((prevState) => !prevState)}
+        title={"setIsWriting"}
+      />
+      <Text>RMS: {features && features.rms}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
