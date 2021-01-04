e9a88c04832fecf8f033e668966b774f6d3ef4f1
YuraBobeshko <gorobech55@gmail.com>
2021-01-04 11:12:52 +0200

    sol

--- AudioAnalyser.js
+++ AudioAnalyser.js
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const AudioAnalyser = ({ audio }) => {
-  const audioContext = new (window.AudioContext || window.webkitAudioContext)(),
-    source = audioContext.createMediaStreamSource(audio),
-    analyser = audioContext.createAnalyser(),
-    processor = audioContext.createScriptProcessor(2048, 1, 1);
+  useEffect(() => {
+    const audioContext = new (window.AudioContext ||
+        window.webkitAudioContext)(),
+      source = audioContext.createMediaStreamSource(audio),
+      analyser = audioContext.createAnalyser(),
+      processor = audioContext.createScriptProcessor(2048, 1, 1);
 
-  let dataArray;
+    let dataArray;
 
-  analyser.fftSize = 32;
-  source.connect(analyser);
-  source.connect(processor);
-  //   analyser.connect(audioContext.destination); for hearing himself
-  processor.connect(audioContext.destination);
-  dataArray = new Uint8Array(analyser.frequencyBinCount);
+    analyser.fftSize = 32;
+    source.connect(analyser);
+    source.connect(processor);
+    //   analyser.connect(audioContext.destination); for hearing himself
+    processor.connect(audioContext.destination);
+    dataArray = new Uint8Array(analyser.frequencyBinCount);
 
-  processor.onaudioprocess = () => {
-    analyser.getByteFrequencyData(dataArray);
-    console.log(dataArray);
-  };
+    processor.onaudioprocess = () => {
+      analyser.getByteFrequencyData(dataArray);
+      console.log(dataArray);
+    };
+    return () => {
+      processor.onaudioprocess = null;
+    };
+  });
 
   return null;
 };
